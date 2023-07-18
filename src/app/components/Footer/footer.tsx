6dc945f993222860131359b1792ea875d202f28a
vov4ik533 <vov4ik533@gmail.com>
2023-07-18 19:43:39 +0300

    add Footer

---
+++ src/app/components/Footer/footer.tsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <p className="">Название сайта</p>
+      </div>
+      <div>
+        <p className="">© 2022 Название сайта. Все права защищены.</p>
+      </div>
+    </footer>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <div className="main"></div>
+      <Footer />
     </>
   );
 }
