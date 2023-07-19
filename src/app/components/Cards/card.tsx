6327a1e35d0a626ebea27d7461e724a69da17831
vov4ik533 <vov4ik533@gmail.com>
2023-07-19 15:41:17 +0300

    add varios img, need to fix display IMG, NOT WORK

--- src/app/components/Cards/card.tsx
+++ src/app/components/Cards/card.tsx
@@ -1,16 +1,26 @@
 import Image from "next/image";
 import styles from "./card.module.css";
-import testImage from "../../../assets/test-image.png";
 
-export default function Cards() {
+export default function Cards({ allPhones }) {
   return (
     <>
-      <div className={styles.card}>
-        <Image className={styles.cardImage} src={testImage} alt="Phone" />
-        <h3>Some Shit</h3>
-        <p>Description for some shit</p>
-        <button>Buy this shit</button>
-      </div>
+      {allPhones.map((phone) => (
+        <div key={phone.id} className={styles.card}>
+          <Image
+            className={styles.cardImage}
+            src={phone.img}
+            alt={phone.name}
+            width={200}
+            height={200}
+          />
+          <h3>{phone.name}</h3>
+          <p>Brand: {phone.brand}</p>
+          <p>Price: ${phone.price}</p>
+          <p>Memory: {phone.memory}GB</p>
+          <p>Color: {phone.color}</p>
+          <button>Buy this phone</button>
+        </div>
+      ))}
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
 import Cards from "./components/Cards/card";
+import allPhones from "./db-phone/phone";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       <Header />
       <div className={styles.main}>
         <div className={styles.cardContainer}>
-          <Cards />
+          <Cards allPhones={allPhones} />
         </div>
       </div>
       <Footer />
