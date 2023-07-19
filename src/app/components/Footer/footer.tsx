c8f262a6603cb62f21e5877f68d892179d07b674
Dmitry <dmitry.kremenetsky1@gmail.com>
2023-07-19 13:13:53 +0300

    fix: change styles and code component

--- src/app/components/Footer/footer.tsx
+++ src/app/components/Footer/footer.tsx
@@ -1,9 +1,9 @@
-import "./footer.css";
+import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="privacy">
+    <footer className={styles.footer}>
+      <div className={styles.privacy}>
         <p className="">© 2023 Online Store. All rights reserved.</p>
         <p className="">Online Store</p>
       </div>
