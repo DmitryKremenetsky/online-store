3be269f5df991cbeaab7899e01f4041921e8d3f8
Dmitry Kremenetsky <73497019+DmitryKremenetsky@users.noreply.github.com>
2023-07-20 14:43:06 +0300

    feat: add filter func (#13)
    * feat: add filter func
    
    * fix: fix pull

--- src/app/components/Filters/filter.tsx
+++ src/app/components/Filters/filter.tsx
@@ -1,52 +1,59 @@
 "use client";
 
 import { useState } from "react";
+import allPhones from "@/app/db-phone/phone";
 import styles from "./filter.module.css";
 
 export default function Filters() {
-  const [minPrice, setMinPrice] = useState(0);
-  const [maxPrice, setMaxPrice] = useState(1000);
-  const memoryOptions = ["32GB", "64GB", "128GB", "256GB"];
-  const colorOptions = ["Red", "Blue", "Green", "Yellow"];
+  const [selectedBrand, setSelectedBrand] = useState("All");
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+  const [selectedMemory, setSelectedMemory] = useState([]);
+  const [selectedColor, setSelectedColor] = useState("All");
+  const [filteredPhones, setFilteredPhones] = useState(allPhones);
 
   return (
     <>
       <div className={styles.sidebar}>
         <h3>Filters</h3>
-        <div className={styles.priceContainer}>
-          <input
-            type="range"
-            min={0}
-            max={1000}
-            value={minPrice}
-            onChange={(e) => setMinPrice(parseInt(e.target.value))}
-          />
-          <div>Min price ${minPrice}</div>
-          <input
-            type="range"
-            min={0}
-            max={1000}
-            value={maxPrice}
-            onChange={(e) => setMaxPrice(parseInt(e.target.value))}
-          />
-          <div>Max price ${maxPrice}</div>
+        <div className={styles.brand}>
+          <h4>Brand</h4>
+          <ul className={styles.brandList}>
+            <li>All</li>
+            <li>Apple</li>
+            <li>Samsung</li>
+          </ul>
         </div>
-        <div className={styles.memoryContainer}>
-          {memoryOptions.map((option, index) => (
-            <label key={index}>
-              <input type="checkbox" value={option} />
-              {option}
-            </label>
-          ))}
+        <div className={styles.price}>
+          <h4>Price</h4>
+          <div className={styles.priceList}>
+            <input type="number" placeholder="min" />
+            <span> - </span>
+            <input type="number" placeholder="max" />
+          </div>
         </div>
-        <div className={styles.colorContainer}>
-          {colorOptions.map((color, index) => (
-            <div
-              key={index}
-              className={styles.colorOption}
-              style={{ backgroundColor: color.toLowerCase() }}
-            ></div>
-          ))}
+        <div className={styles.memory}>
+          <h4>Memory</h4>
+          <div className={styles.memoryList}>
+            <input type="checkbox" />
+            <label>64gb</label>
+            <input type="checkbox" />
+            <label>128gb</label>
+            <input type="checkbox" />
+            <label>256gb</label>
+          </div>
+        </div>
+        <div className={styles.color}>
+          <h4>Color</h4>
+          <div className={styles.colorList}>
+            <div className={styles.colorList}>
+              <button>All</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button>Apply Filters</button>
+          <button>Reset Filters</button>
         </div>
       </div>
     </>
